4d2860cc65eb9d64a068763419d56dedb6e99244
butayatoriyama57 <donghe.tain0402@gmail.com>
2025-01-04 00:32:19 +0900

    2025/01/04

--- js/jquery.nav.js
+++ js/jquery.nav.js
@@ -1,20 +1,6 @@
-/*
- * jQuery One Page Nav Plugin
- * http://github.com/davist11/jQuery-One-Page-Nav
- *
- * Copyright (c) 2010 Trevor Davis (http://trevordavis.net)
- * Dual licensed under the MIT and GPL licenses.
- * Uses the same license as jQuery, see:
- * http://jquery.org/license
- *
- * @version 3.0.0
- *
- * Example usage:
- * $('#nav').onePageNav({
- *   currentClass: 'current',
- *   changeHash: false,
- *   scrollSpeed: 750
- * });
+/*butayatoriyama
+*toriyama57.github.io
+LE CHI THANH
  */
 
 ;(function($, window, document, undefined){
